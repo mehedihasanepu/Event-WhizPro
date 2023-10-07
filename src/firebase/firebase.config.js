67524e280e18b615a435af216282213e456bbcc4
mehedihasanepu <mehedihasanepu123@gmail.com>
2023-10-07 16:57:28 +0600

    add react router, tailwindcss, daisyUI and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjWUiaU9jeni4pakBsVB-UT_t27LNfOmM",
+  authDomain: "event-whizpro.firebaseapp.com",
+  projectId: "event-whizpro",
+  storageBucket: "event-whizpro.appspot.com",
+  messagingSenderId: "173459421485",
+  appId: "1:173459421485:web:4836f04be454cb25c33ba4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
